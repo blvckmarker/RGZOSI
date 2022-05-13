88e5aede14ae94eb980104ee4a8a8b54cebfe521
EgorYankovsky <egeryankov@gmail.com>
2022-05-13 23:34:50 +0700

    18+

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Clipboard
+{
+   class Buffer
+   {
+      private int head, tail, size;
+
+   }
+    class Clipboard
+    {
+        
+ 
+
+    }
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Emultor());
+        }
+     
+
+
+        
+    }
+}
